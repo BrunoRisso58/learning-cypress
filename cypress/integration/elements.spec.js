006af4563fab260085ba275280c3151ddee48446
Bruno Risso <brunorisso58@gmail.com>
2022-06-21 18:52:28 -0300

    Text Fields

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,6 +1,16 @@
 /// <reference types="cypress" />
 
 describe('Work with basic elements', () => {
+    before(() => {
+        // vai para a página
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+    })
+
+    beforeEach(() => {
+        // vai para a página
+        cy.reload();
+    })
+
     it('Text', () => {
         // vai para a página
         cy.visit('https://wcaquino.me/cypress/componentes.html');
@@ -11,7 +21,7 @@ describe('Work with basic elements', () => {
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...');
     })
 
-    it.only('Links', () => {
+    it('Links', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html');
         cy.get('[href="#"]').click();
         cy.get('#resultado').should('have.text', 'Voltou!');
@@ -20,5 +30,28 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.not.text', 'Voltou!');
         cy.contains('Voltar').click();
         cy.get('#resultado').should('have.text', 'Voltou!');
+    })
+
+    it.only('TextFields', () => {
+        cy.get('#formNome')
+            .type('Cypress Text')
+            // .should('have.text', 'Cypress Text') // não funciona pois o texto em um input HTML fica no atributo value
+            .should('have.value', 'Cypress Text');
+
+        cy.get('#elementosForm\\:sugestoes') // quando tiver \, colocar \\, pois \ já é um catacter especial
+            .type('textarea')
+            .should('have.value', 'textarea');
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???');
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', { delay: 100 })
+            .should('have.value', 'acerto')
     })
 })
