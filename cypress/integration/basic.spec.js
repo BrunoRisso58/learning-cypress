0604a3b4f24f5fb0b88fa2597dcf440b8d301461
Bruno Risso <brunorisso58@gmail.com>
2022-06-21 16:02:26 -0300

    Finishing the Basic Cypress section

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -9,14 +9,24 @@ describe('Cypress basics', () => {
         // const title = cy.title()
         // console.log(title);
 
+        cy.pause();
+
         // essa é a forma que iremos usar, pois o cy.title() retorna uma promise
         // o cypress fica tentando verificar a asserção até que o valor do título se transforme no que queremos ou que dê timeout (4s)
         cy.title().should('be.equal', 'Campo de Treinamento');
-        cy.title().should('contain', 'Campo');
+        cy.title().should('contain', 'Campo').debug(); // o debug é bom para pegar mais detalhes sobre algum ponto
 
         // mesma coisa do outro exemplo
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo');
+    })
+
+    it('Should find and interact with an element', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html');
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!');
     })
 })
