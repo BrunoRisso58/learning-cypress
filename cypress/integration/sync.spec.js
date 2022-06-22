86bf074b8a00bebcf9630b795712dd7b1298aa87
Bruno Risso <brunorisso58@gmail.com>
2022-06-22 15:19:02 -0300

    Retentativas

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -17,7 +17,20 @@ describe('Esperas...', () => {
     it('Deve aguardar elemento estar disponível', () => {
         cy.get('#novoCampo').should('not.exist')
         cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
         cy.get('#novoCampo').type('funciona')
+    })
+
+    it.only('Deve fazer retrys', () => {
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+            .should('not.exist')
+            // quando eu garanto que o elemento não existe, não faz sentido ele retornar o mesmo elemento, por isso o should('exist') não funciona
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('funciona')
+            .should('have.value', 'funciona')
     })
 
 })
