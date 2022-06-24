cf5fed6a8ee6893de011a3b86e07ee6071bcc202
Bruno Risso <brunorisso58@gmail.com>
2022-06-24 15:56:11 -0300

    each

--- cypress/integration/dynamic.spec.js
+++ cypress/integration/dynamic.spec.js
@@ -25,4 +25,23 @@ describe('Dynamic tests', () => {
             cy.get('#resultado').should('contain', 'Cadastrado!')
         })
     })
+
+    it.only('Deve selecionar todos usando o each', () => {
+        cy.get('#formNome').type('Usuario')
+        cy.get('[data-cy=dataSobrenome]').type('Qualquer')
+        cy.get(`[name=formSexo][value=F]`).click()
+        cy.get('[name=formComidaFavorita]').each($el => { // executa a função para cada elemento que o get pegou
+            // só clica se o valor do elemento for diferente de 'vegetariano'
+            if ($el.val() != 'vegetariano') {
+                cy.wrap($el).click()
+            }
+        })
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+
+        // cy.get('#formCadastrar').click()
+        // cy.get('#resultado').should('contain', 'Cadastrado!')
+
+        cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
+    })
 })
